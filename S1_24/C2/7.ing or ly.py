fa751be022336d35a3c8893f9a974dc8a55a81b2
raniya21 <96460388+raniya21@users.noreply.github.com>
2022-03-03 00:29:20 -0800

    Delete S1_24 directory

--- S1_24/C2/7.ing or ly.py
+++
@@ -1,7 +0,0 @@
-string=input("enter a word")
-last=string[-3:]
-if(last=='ing'):
-    string+="ly"
-else:
-    string+="ing"
-print(string)
--- S1_24/C3/1.py
+++
@@ -1,6 +0,0 @@
-import math
-a=int(input("enter the number:"))
-print(math.factorial(a))
-b=int(input("enter the number for its sqrt:"))
-print(math.sqrt(b))
- 
